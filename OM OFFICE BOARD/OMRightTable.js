215a18fa7ace11cf2ac3cb4699a62ff8b7c21aa2
ennsimon <41064817+ennsimon@users.noreply.github.com>
2018-07-10 09:15:26 -0500

    Add files via upload

---
+++ OM OFFICE BOARD/OMRightTable.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+
+import './OMRightTable.css';
+
+
+
+class OMLeftTable extends Component {
+    render() {
+      return (
+        <div >
+          <table  id="rightTable">
+            <tbody>
+            <tr id ="header1">
+                <td id="a">Sorter1 Info </td>
+                <td id="a">PKD</td>
+                <td id="a">SRT</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td >0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td id="a">Sorter2 Info </td>
+                <td id="a">PKD</td>
+                <td id="a">SRT</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+            <tr>
+                <td>0 </td>
+                <td>0.0</td>
+                <td>0.0</td>
+                
+            </tr>
+                
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
+  
+  export default OMLeftTable;
