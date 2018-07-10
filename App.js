dc10a3654485e446e98e3bb3dd0acdc7337436e1
ennsimon <41064817+ennsimon@users.noreply.github.com>
2018-07-10 15:48:41 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './WaveBoard.js';
+import WaveBoard from './WaveBoard.js';
+import Clock from './clock.js';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <WaveBoard/>
+        
+        
+
+         </div>
+);
+  }
+}
+
+export default App;
---
+++ WaveBoard.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+import './WaveBoard.css';
+
+
+
+class WaveBoard extends Component {
+    render() {
+      return (
+        <div >
+          <table  align="left" id="leftTable">
+            <tbody>
+              <tr>
+                <th>Wave</th>
+                <th>PKD</th>
+                <th>SRT</th>
+                <th>MT</th>
+                <th>CHT</th>
+                <th>DUR</th>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0.0</td>
+                <td>0.0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0.0</td>
+                <td>0.0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0.0</td>
+                <td>0.0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0.0</td>
+                <td>0.0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0.0</td>
+                <td>0.0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0.0</td>
+                <td>0.0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <td>0</td>
+                <td>0.0</td>
+                <td>0.0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <th>AVAILABLE CHUTES:</th>
+              </tr>
+              
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
+  
+  export default WaveBoard;
